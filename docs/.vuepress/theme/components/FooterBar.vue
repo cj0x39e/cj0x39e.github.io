fb82fef4f55c59c46c82faf013654edfa3f4b373
陶智 <taozhi@tuliu.com>
2021-07-24 14:49:37 +0800

    update footer style

--- docs/.vuepress/theme/components/FooterBar.vue
+++ docs/.vuepress/theme/components/FooterBar.vue
@@ -1,11 +1,15 @@
 <template>
   <footer class="footer">
-    <p class="text">I know nothing...</p>
+    <p class="text">© {{ thisYear }} cj0x39e</p>
   </footer>
 </template>
 
 <script>
 export default {
-  computed: {},
+  computed: {
+    thisYear() {
+      return new Date().getFullYear();
+    },
+  },
 };
 </script>
